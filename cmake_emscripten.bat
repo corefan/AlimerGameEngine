9b03f48f284628c2ba132727651316c7f46bef46
Amer Koleci <amerkoleci@gmail.com>
2017-06-23 19:28:40 +0200

    Add emscripte toolchain support and bat files for building Android and Emscripten.

---
+++ cmake_emscripten.bat
@@ -0,0 +1,5 @@
+set "SOURCE=%~dp0"
+set "SOURCE=%SOURCE:~0,-1%"
+set "BUILD=build/emscripten"
+if "%TOOLCHAINS%" == "" set "TOOLCHAINS=%SOURCE%\CMake\Toolchains"
+cmake -E make_directory "%BUILD%" && cmake -E chdir "%BUILD%" cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAINS%\emscripten.toolchain.cmake" -DCMAKE_BUILD_TYPE=Release "%SOURCE%" && cmake --build "%BUILD%"
