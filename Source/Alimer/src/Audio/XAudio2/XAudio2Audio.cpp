67fc9d806d356c573cacc6b6f459dca9ae474c42
Amer Koleci <amerkoleci@gmail.com>
2017-06-26 19:47:42 +0200

    Working XAudio backend with 2.9, 2.8 and 2.7 support. X3DAudio support also.

--- Source/Alimer/src/Audio/XAudio2/XAudio2Audio.cpp
+++ Source/Alimer/src/Audio/XAudio2/XAudio2Audio.cpp
@@ -133,6 +133,236 @@ namespace Alimer
 			ALIMER_LOGERROR("Failed to initialize Media Foundation, Error: %i", hr);
 		}
 
+		// Create XAudio2 engine
+		UINT32 eflags = 0;
+#if WINDOWS_USE_DYNAMIC_LIB
+		uint16_t apiMinorVersion = 9;
+		_xAudioDLL = LoadLibraryExW(L"XAudio2_9", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+
+		if (!_xAudioDLL)
+		{
+			_xAudioDLL = LoadLibraryExW(L"XAudio2_8", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+			apiMinorVersion = 8;
+		}
+
+		const bool forceUseXAudio27 = false;
+		const bool enableDebug = false;
+		if (!_xAudioDLL || forceUseXAudio27)
+		{
+			// Windows 7
+			apiMinorVersion = 7;
+			_usingXAudio27 = true;
+			_xAudioDLL = enableDebug ? LoadLibraryEx(L"XAudioD2_7.DLL", nullptr, 0x00000800 /* LOAD_LIBRARY_SEARCH_SYSTEM32 */) : LoadLibraryExW(L"XAudio2_7.DLL", nullptr, 0x00000800 /* LOAD_LIBRARY_SEARCH_SYSTEM32 */);
+			_x3DAudioDLL = LoadLibraryExW(L"X3DAudio1_7", nullptr, LOAD_LIBRARY_SEARCH_SYSTEM32);
+
+			if (!_xAudioDLL)
+			{
+				ALIMER_LOGERROR("ERROR: XAudio 2.7 not installed on system (install the DirectX End-user Runtimes (June 2010))\n");
+				return false;
+			}
+		}
+
+		if (_usingXAudio27)
+		{
+			hr = XAudio27CreateProc(xAudio2.ReleaseAndGetAddressOf(), eflags, XAUDIO2_DEFAULT_PROCESSOR);
+		}
+		else
+		{
+			_x3DAudioDLL = _xAudioDLL;
+
+			PFN_XAudioCreate XAudio2CreateFunc = reinterpret_cast<PFN_XAudioCreate>(GetProcAddress(_xAudioDLL, "XAudio2Create"));
+			if (!XAudio2CreateFunc)
+				return false;
+
+			hr = XAudio2CreateFunc(xAudio2.ReleaseAndGetAddressOf(), 0, XAUDIO2_DEFAULT_PROCESSOR);
+		}
+
+		X3DAudioInitializeFunc = (PFN_X3DAudioInitialize)GetProcAddress(_x3DAudioDLL, "X3DAudioInitialize");
+		if (!X3DAudioInitializeFunc)
+			return S_FALSE;
+
+		X3DAudioCalculateFunc = (PFN_X3DAudioCalculate)GetProcAddress(_x3DAudioDLL, "X3DAudioCalculate");
+		if (!X3DAudioCalculateFunc)
+			return S_FALSE;
+
+#else
+		hr = XAudio2Create(&xaudio2, eflags);
+#endif
+
+		if (FAILED(hr))
+		{
+			ALIMER_LOGERROR("Failed to initialize XAudio2.%d, error: %i", apiMinorVersion, hr);
+			return false;
+		}
+
+		if (enableDebug)
+		{
+			XAUDIO2_DEBUG_CONFIGURATION debug = {};
+			debug.TraceMask = XAUDIO2_LOG_ERRORS | XAUDIO2_LOG_WARNINGS;
+			debug.BreakMask = XAUDIO2_LOG_ERRORS;
+			xAudio2->SetDebugConfiguration(&debug, nullptr);
+			if (apiMinorVersion == 9)
+			{
+				ALIMER_LOGINFO("XAudio 2.9 debugging enabled\n");
+			}
+			else if (apiMinorVersion == 8)
+			{
+				// To see the trace output, you need to view ETW logs for this application:
+				//    Go to Control Panel, Administrative Tools, Event Viewer.
+				//    View->Show Analytic and Debug Logs.
+				//    Applications and Services Logs / Microsoft / Windows / XAudio2. 
+				//    Right click on Microsoft Windows XAudio2 debug logging, Properties, then Enable Logging, and hit OK 
+				ALIMER_LOGINFO("XAudio 2.8 debugging enabled\n");
+			}
+			else
+			{
+				// To see the trace output, see the debug output channel window
+				ALIMER_LOGINFO("XAudio 2.7 debugging enabled\n");
+			}
+		}
+
+		/*hr = xAudio2->RegisterForCallbacks(&mEngineCallback);
+		if (FAILED(hr))
+		{
+		xAudio2.Reset();
+		return hr;
+		}*/
+
+		const wchar_t* deviceId = nullptr;
+		if (!_usingXAudio27)
+		{
+			const AUDIO_STREAM_CATEGORY category = AudioCategory_GameEffects;
+			hr = xAudio2->CreateMasteringVoice(&_masterVoice, XAUDIO2_DEFAULT_CHANNELS, XAUDIO2_DEFAULT_SAMPLERATE,
+				0, deviceId, nullptr, category);
+
+			if (FAILED(hr))
+			{
+				xAudio2.Reset();
+				return hr;
+			}
+
+			DWORD dwChannelMask;
+			hr = _masterVoice->GetChannelMask(&dwChannelMask);
+			if (FAILED(hr))
+			{
+				SAFE_DESTROY_VOICE(_masterVoice);
+				xAudio2.Reset();
+				return hr;
+			}
+
+			XAUDIO2_VOICE_DETAILS details;
+			_masterVoice->GetVoiceDetails(&details);
+
+			_masterChannelMask = dwChannelMask;
+			_masterChannels = details.InputChannels;
+			_masterRate = details.InputSampleRate;
+
+			// StartEngine
+			hr = xAudio2->StartEngine();
+			if (FAILED(hr))
+			{
+				xAudio2.Reset();
+				return hr;
+			}
+		}
+		else
+		{
+			UINT32 count = 0;
+			hr = IXAudio27GetDeviceCount(xAudio2.Get(), &count);
+			if (FAILED(hr))
+			{
+				xAudio2.Reset();
+				return hr;
+			}
+
+			if (!count)
+			{
+				xAudio2.Reset();
+				return HRESULT_FROM_WIN32(ERROR_NOT_FOUND);
+			}
+
+			UINT32 devIndex = 0;
+			if (deviceId)
+			{
+				// Translate device ID back into device index
+				devIndex = UINT32(-1);
+				for (UINT32 j = 0; j < count; ++j)
+				{
+					XAUDIO2_DEVICE_DETAILS details;
+					hr = IXAudio27GetDeviceDetails(xAudio2.Get(), j, &details);
+					if (SUCCEEDED(hr))
+					{
+						if (wcsncmp(deviceId, details.DeviceID, 256) == 0)
+						{
+							devIndex = j;
+							_masterChannelMask = details.OutputFormat.dwChannelMask;
+							break;
+						}
+					}
+				}
+
+				if (devIndex == UINT32(-1))
+				{
+					xAudio2.Reset();
+					return HRESULT_FROM_WIN32(ERROR_NOT_FOUND);
+				}
+			}
+			else
+			{
+				// No search needed
+				XAUDIO2_DEVICE_DETAILS details;
+				hr = IXAudio27GetDeviceDetails(xAudio2.Get(), 0, &details);
+				if (FAILED(hr))
+				{
+					xAudio2.Reset();
+					return hr;
+				}
+
+				_masterChannelMask = details.OutputFormat.dwChannelMask;
+			}
+
+			hr = IXAudio2CreateMasteringVoice(xAudio2.Get(), devIndex, &_masterVoice);
+			if (FAILED(hr))
+			{
+				xAudio2.Reset();
+				return hr;
+			}
+
+			XAUDIO2_VOICE_DETAILS details;
+			_masterVoice->GetVoiceDetails(&details);
+
+			_masterChannels = details.InputChannels;
+			_masterRate = details.InputSampleRate;
+
+			// StartEngine
+			hr = IXAudio27StartEngine(xAudio2.Get());
+			if (FAILED(hr))
+			{
+				xAudio2.Reset();
+				return hr;
+			}
+		}
+
+		ALIMER_LOGINFO("Mastering voice has %u channels, %u sample rate, %08X channel mask\n", _masterChannels, _masterRate, _masterChannelMask);
+
+		// Setup 3D audio
+		const float SPEEDOFSOUND = X3DAUDIO_SPEED_OF_SOUND;
+
+#if WINDOWS_USE_DYNAMIC_LIB
+		hr = X3DAudioInitializeFunc(_masterChannelMask, SPEEDOFSOUND, _X3DAudio);
+#else
+		hr = X3DAudioInitialize(_masterChannelMask, SPEEDOFSOUND, _X3DAudio);
+		if (FAILED(hr))
+		{
+			SAFE_DESTROY_VOICE(_reverbVoice);
+			SAFE_DESTROY_VOICE(_masterVoice);
+			//_reverbEffect.Reset();
+			//_volumeLimiter.Reset();
+			xAudio2.Reset();
+			return false;
+		}
+#endif
+
 		return true;
 	}
 }
