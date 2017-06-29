fac8f714ccfd5452d35101c2b81ab4e950d54435
Amer Koleci <amerkoleci@gmail.com>
2017-06-29 21:18:53 +0200

    Add initial Shader GraphicsResource.

---
+++ Source/Alimer/include/Graphics/Shader.h
@@ -0,0 +1,73 @@
+/*
+** Alimer - Copyright (C) 2016-2017 Amer Koleci.
+**
+** This file is subject to the terms and conditions defined in
+** file 'LICENSE.md', which is part of this source code package.
+*/
+
+#pragma once
+
+#include "Graphics/GraphicsResource.h"
+
+namespace Alimer
+{
+	enum class ShaderStage
+	{
+		/**
+		* Vertex shader stage.
+		*/
+		Vertex = 0,
+		/**
+		* Tessellation control shader stage (hull).
+		*/
+		TessellationControl = 1,
+		/**
+		* Tessellation control shader stage (domain).
+		*/
+		TessellationEvaluation = 2,
+		/**
+		* Geometry shader stage.
+		*/
+		Geometry = 3,
+		/**
+		* Fragment shader (pixel).
+		*/
+		Fragment = 4,
+		/**
+		* Compute shader stage.
+		*/
+		Compute = 5,
+		/**
+		* Count of shader types.
+		*/
+		Count
+	};
+
+	class GraphicsDevice;
+
+	/**
+	* Shader.
+	*/
+	class ALIMER_API Shader : public GraphicsResource
+	{
+	protected:
+		/**
+		* Constructor.
+		*/
+		Shader(GraphicsDevice* device, uint32_t codeSize, const uint32_t* code);
+
+	public:
+		/**
+		* Destructor.
+		*/
+		virtual ~Shader();
+		
+	protected:
+		std::vector<uint32_t> spirv;
+
+	private:
+		DISALLOW_COPY_AND_ASSIGN(Shader);
+	};
+
+	using ShaderPtr = RefPtr<Shader>;
+}
---
+++ Source/Alimer/src/Graphics/Shader.cpp
@@ -0,0 +1,22 @@
+/*
+** Alimer - Copyright (C) 2016-2017 Amer Koleci.
+**
+** This file is subject to the terms and conditions defined in
+** file 'LICENSE.md', which is part of this source code package.
+*/
+
+#include "Graphics/Shader.h"
+
+namespace Alimer
+{
+	Shader::Shader(GraphicsDevice* device, uint32_t codeSize, const uint32_t* code)
+		: GraphicsResource(device, GraphicsResourceType::Shader)
+		, spirv(code, code + codeSize)
+	{
+		
+	}
+
+	Shader::~Shader()
+	{
+	}
+}
